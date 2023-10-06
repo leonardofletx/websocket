3d881d8025989bf096f7a8c87069a4fc7cf57a2a
Leonardo <leoardocastri1997@gmail.com>
2023-10-05 21:44:53 -0500

    first commit

---
+++ src/chat/chat.gateway.ts
@@ -0,0 +1,22 @@
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
+
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
+export class ChatGateway {
+  @WebSocketServer()
+  server: Server;
+
+  @SubscribeMessage('identity')
+  async identity(@MessageBody() data: number): Promise<number> {
+    return data;
+  }
+}
